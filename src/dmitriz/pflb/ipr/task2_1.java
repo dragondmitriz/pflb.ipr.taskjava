545845e1bd751322f79cb79aa5a5a093f3177ecd
dragondmitriz <dmitriz.malyshko@mail.ru>
2019-11-19 11:09:35 +0300

    удалил неиспользуемые импорты в задании 2а

--- src/dmitriz/pflb/ipr/task2_1.java
+++ src/dmitriz/pflb/ipr/task2_1.java
@@ -1,15 +1,10 @@
 package dmitriz.pflb.ipr;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class task2_1 {
 
