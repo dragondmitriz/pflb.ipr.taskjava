8d029005a987df000aae35a0458fd59c6e109d5b
dmitriz <dmitriz.malyshko@mail.ru>
2019-12-08 16:05:03 +0300

    fix try-with-resourses #task1

--- src/dmitriz/pflb/ipr/task1.java
+++ src/dmitriz/pflb/ipr/task1.java
@@ -9,37 +9,32 @@ public class task1 {
 
     public static void main(String[] args) {
 
-        try {
-            String fileName = args[0];
-            String prefix = args[1];
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
 
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String prefix = args[1];
 
             int size = 0;
             int index = 0;
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(prefix + index + ".log"),
-                    StandardCharsets.UTF_8);
 
             String line;
             while ((line = reader.readLine()) != null) {
-                size += line.length();
-                if (size >= MAX_SIZE) {
-                    System.out.println("Check max size of file - next new file.");
 
-                    index++;
-                    writer = new OutputStreamWriter(new FileOutputStream(prefix + index + ".log"),
-                            StandardCharsets.UTF_8);
-                    size = 0;
+                try (OutputStreamWriter writer = new OutputStreamWriter(
+                        new FileOutputStream(prefix + index + ".log", true),
+                        StandardCharsets.UTF_8)) {
+
+                    size += line.length();
+                    if (size >= MAX_SIZE) {
+                        System.out.println("Check max size of file - next new file.");
+
+                        index++;
+                        size = 0;
+                    }
+                    writer.write(line + "\n");
                 }
-                writer.write(line + "\n");
             }
 
             System.out.println("Spliting is finished.");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Not found full parameters.\n" +
-                    "Arguments of task 1:\n" +
-                    "\tlog file name\n" +
-                    "\tprefix of log file name after splitting");
         } catch (FileNotFoundException e) {
             System.out.println("File main.log.2014-11-17 is not found.");
         } catch (IOException e) {
